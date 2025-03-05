71a4b2ce5ef116aa9579312044d338d245a459be
Fred56733 <fred.debiase55@gmail.com>
2025-03-05 12:30:28 -0500

    added .env and catInfo.jsx and basic api calling logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
-import { useState } from 'react'
+import { useState,  useEffect } from 'react'
 import './App.css'
+import CatInfo from './components/catInfo'
 
 function App() {
+  const [catData, setCatData] = useState([]);
+  const [bannedCats, setBannedCats] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('https://api.thecatapi.com/v1/breeds', {
+        headers: {
+          'x-api-key': import.meta.env.VITE_CAT_API_KEY
+        }
+      });
+    const data = await response.json();
+    const filteredData = data.filter((cat) => !bannedCats.includes(cat.name));
+    const randomCat = filteredData[Math.floor(Math.random() * filteredData.length)];
+    setCatData(randomCat);
+    } catch (error) {
+      console.error('Error fetching cat data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [bannedCats]);
+
+  const handleBan = (attribute) => {
+    setBannedCats([...bannedCats, attribute]);
+  }
 
   return (
-    <>
-      <body>
-        <div className="app-container">
-          <h3>Veni Venci</h3>
-
-        </div>  
-      </body>
-    </>
-  )
+    <div className="app-container">
+      <h3>Veni Venci</h3>
+      <button onClick={fetchData} disabled={loading}>
+        {loading ? 'Loading...' : 'New Cat'}
+      </button>
+      {catData && <CatInfo catData={catData} handleBan={handleBan}/>}
+    </div>  
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/catInfo.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CatInfo = ({catData, handleBan}) => {
+    return (
+        <div className="catInfo">
+            <h4>{catData.name}</h4>
+            <p>Origin: {catData.origin}</p>
+            <p>Temperament: {catData.temperament}</p>
+            <p>Life Span: {catData.life_span}</p>
+            {catData.image && <img src={catData.image.url} alt={catData.name}/>}
+            <button onClick={() => handleBan(catData.id)}>Ban</button>
+        </div>
+    );
+    };
+
+export default CatInfo;
