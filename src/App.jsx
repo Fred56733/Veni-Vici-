a590c4415f27f89ab52ccdeab3774f4074cc70ed
Fred56733 <fred.debiase55@gmail.com>
2025-03-05 11:10:56 -0500

    Start of react project 4

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <body>
+        <div className="app-container">
+          <h3>Veni Venci</h3>
+
+        </div>  
+      </body>
+    </>
+  )
+}
+
+export default App
